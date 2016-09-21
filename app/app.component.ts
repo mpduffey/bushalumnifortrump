3d42ad6a0576400ca27c31b5c97735eb7d4cfe4c
Mike Duffey <mpduffey@gmail.com>
2016-09-21 01:32:55 +0000

    generify2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<single-page-scrolling [app]="app"></single-page-scrolling>`
 })
-export class AppComponent { }
+
+export class AppComponent {
+	app = {
+		navMenu:	[
+			{link: "#intro", label: "Home"},
+			{link: "#about", label: "About"},
+			{link: "#calendar", label: "Calendar"},
+			{link: "#membership", label: "Membership"},
+			{link: "https://goo.gl/7c5jOW", label: "Sign-up", external: true},
+			{link: "#contact", label: "Contact"}
+		],
+		intro:		{
+			title:				"Republican Presidential Appointees for Trump",
+			buttonTitle:	"I Support Donald Trump! Sign Me Up!",
+			buttonLink:		"https://goo.gl/7c5jOW"
+		},
+		about:		{
+			img:	      "img/trump-2.jpg",
+      smallTitle: "America First",
+      boldTitle:  "Republican Presidential Appointees ",
+      lightTitle: "for Trump",
+			text:	      "We are former Presidential Appointees in the Ronald Reagan, George H. W. Bush and George W. Bush administations. We favor an America with a strong national defense, tax and economic policy to support strong economic growth, protection of our freedoms, and conservative Supreme Court appointees that will interpret the law with strict constitutional adherence. Therefore, we support Donald J. Trump and Governor Mike Pence for President and Vice President of the United States."
+		},
+		calendar:	{
+			events:				[
+				{
+					date:				new Date(2016,8,17,11),
+					endDate:		null,
+					name:				"Constitution Day Celebration",
+					location:		"La Crosse, Outagamie, Waukesha, Shawano, Wausau, West Bend, Milwaukee, and Dane County Victory Centers",
+					contact:		{
+						name:				"Charlotte Rasmussen",
+						email:			"charlotter@ceas.coop",
+						phone:			"(715) 551-9724"
+					}
+				},
+			]
+		},
+		contact:	{
+			text:			"Contact Republican Appointees for Trump",
+			email:		"mpduffey@gmail.com"
+		},
+		footer:		{
+			text:			"Paid For By Donald J. Trump for President"
+		}
+	};
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,25 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule}             from '@angular/core';
+import {BrowserModule}        from '@angular/platform-browser';
+import {HttpModule}           from '@angular/http';
+import {ReactiveFormsModule}  from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import {AppComponent}         from './app.component';
+import {SinglePageScrolling}  from 'modules/single-page-scrolling/single-page-scrolling';
+import {NavMenu}              from 'modules/nav-menu/nav-menu';
+import {SpsIntroSection}      from 'modules/sps-intro-section/sps-intro-section';
+import {SpsAboutSection}      from 'modules/sps-about-section/sps-about-section';
+import {SpsEventCalendar}     from 'modules/sps-event-calendar/sps-event-calendar';
+import {SpsMembership}        from 'modules/sps-membership/sps-membership';
+import {SpsContactSection}    from 'modules/sps-contact-section/sps-contact-section';
+import {SpsFooter}            from 'modules/sps-footer/sps-footer';
+import {ImgGallery}           from 'modules/img-gallery/img-gallery';
+import {Filter}               from 'modules/filter-pipe/filter-pipe';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports:      [BrowserModule, HttpModule, ReactiveFormsModule],
+  exports:      [SinglePageScrolling, NavMenu, SpsIntroSection, SpsAboutSection, SpsEventCalendar, ImgGallery, SpsMembership, SpsContactSection, Filter, SpsFooter],
+  declarations: [AppComponent, SinglePageScrolling, NavMenu, SpsIntroSection, SpsAboutSection, SpsEventCalendar, ImgGallery, SpsMembership, SpsContactSection, Filter, SpsFooter],
+  bootstrap:    [AppComponent]
 })
+
 export class AppModule { }
